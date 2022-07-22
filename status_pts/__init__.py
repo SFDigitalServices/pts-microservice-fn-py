5fb3c94aea24c825e0f47abdd412725ea66fcf18
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-07-21 17:26:04 -0700

    update status/pts

--- status_pts/__init__.py
+++ status_pts/__init__.py
@@ -15,34 +15,31 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Status PTS processed a request.')
 
     try:
-        usr = os.environ.get('PTS_USERNAME')
-        pwd = os.environ.get('PTS_PASSWORD')
-        cns = os.environ.get('PTS_CONNECTSTRING')
-        out = None
-        lib_dir = os.environ.get('ORACLE_LIB_DIR')
-        oracledb.init_oracle_client(lib_dir)
-        with oracledb.connect(user=usr, password=pwd, dsn=cns) as connection:
-            with connection.cursor() as cursor:
-                print(oracledb.clientversion())
-                sql = """select sysdate from dual"""
-                for rtn in cursor.execute(sql):
-                    print(rtn)
-                    if rtn:
-                        out = "OK"
-
-        response = Response()
-        if req.get_body() and len(req.get_body()):
-            response.status_code = 202
-            # pylint: disable=protected-access
-            response._content = b'"202 Accepted"'
-        else:
-            response.status_code = 200
-            # pylint: disable=protected-access
-            response._content = b'"200 OK"'
-
         headers = {
             "Access-Control-Allow-Origin": "*"
         }
+        response = Response()
+        out = None
+        if req.get_body() and len(req.get_body()):
+            response = requests.get('https://ifconfig.me')
+            out = str(response.text)
+        else:
+            usr = os.environ.get('PTS_USERNAME')
+            pwd = os.environ.get('PTS_PASSWORD')
+            cns = os.environ.get('PTS_CONNECTSTRING')
+
+            lib_dir = os.environ.get('ORACLE_LIB_DIR')
+            oracledb.init_oracle_client(lib_dir)
+            with oracledb.connect(user=usr, password=pwd, dsn=cns) as connection:
+                with connection.cursor() as cursor:
+                    print(oracledb.clientversion())
+                    sql = """select sysdate from dual"""
+                    for rtn in cursor.execute(sql):
+                        print(rtn)
+                        if rtn:
+                            response.status_code = 200
+                            out = "200 OK"
+
         return func_json_response(response, headers, "message", out)
 
     #pylint: disable=broad-except
