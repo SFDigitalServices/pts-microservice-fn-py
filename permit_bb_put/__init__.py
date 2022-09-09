d52bc2096ea7aa92379b6e614d88a8231ef495c2
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-09-09 09:40:26 -0700

    permit bluebeam update

---
+++ permit_bb_put/__init__.py
@@ -0,0 +1,71 @@
+""" permit_bb init file """
+import os
+import json
+import logging
+import traceback
+import requests
+import jsend
+import azure.functions as func
+from requests.models import Response
+from shared_code.common import func_json_response
+from shared_code.oracle import get_oracle_connection
+
+#pylint: disable=too-many-locals
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    main function for permit_post
+    """
+    logging.info('Permit Bluebeam PUT processed a request.')
+
+    try:
+        response = Response()
+        out = None
+        headers = {
+            "Access-Control-Allow-Origin": "*"
+        }
+
+        if req.get_json() and len(req.get_json()):
+            response.status_code = 200
+            req_json = req.get_json()
+
+            sp_gen_permit = [
+                "P_APPLICATION_NUMBER",
+                "P_BLUEBEAM_PROJ_NO",
+                "P_STATUS",
+                "P_MSG"
+            ]
+
+            connection = get_oracle_connection()
+            with connection.cursor() as cursor:
+                data_json =  req_json
+                data_json["P_STATUS"] = cursor.var(str)
+                data_json["P_MSG"] = cursor.var(str)
+
+                params = []
+                for field in sp_gen_permit:
+                    value = data_json[field] if field in data_json else ''
+                    params.append(value)
+                #bool_result = cursor.callfunc("pts.sp_gen_permit", int, params)
+                #print(bool_result)
+                cursor.callproc("pts.sp_ds_update_bluebeamprj", params)
+                print("p_status: " + str(data_json["P_STATUS"].getvalue()))
+                print("p_msg: " + str(data_json["P_MSG"].getvalue()))
+
+                if data_json["P_STATUS"].getvalue():
+                    out = data_json["P_STATUS"].getvalue() + " " + data_json["P_MSG"].getvalue()
+
+            print(out)
+        else:
+            response.status_code = 200
+
+            out = "200 OK PUT"
+
+        return func_json_response(response, headers, "message", out)
+
+    #pylint: disable=broad-except
+    except Exception as err:
+        logging.error("Status HTTP error occurred: %s", traceback.format_exc())
+
+        msg_error = f"This endpoint encountered an error. {err}"
+        func_response = json.dumps(jsend.error(msg_error))
+        return func.HttpResponse(func_response, status_code=500)
