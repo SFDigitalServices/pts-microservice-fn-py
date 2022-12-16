0ec5bb30131beca98d32d4f42f759d9cd248d917
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-12-16 08:21:42 -0800

    adding test fixtures and update permit_get test

--- tests/test_permit_get.py
+++ tests/test_permit_get.py
@@ -1,14 +1,18 @@
 """ Test for status/http endpoint """
+# pylint: disable=redefined-outer-name,unused-argument,unused-import
 import json
 from unittest.mock import patch
 import azure.functions as func
+from tests.fixtures import mock_env_access_key, mock_env_no_access_key, CLIENT_HEADERS
 from permit_get import main
 
-def test_permit_get_function():
+
+def test_permit_get_function(mock_env_access_key):
     """ test_permit_get_function """
     # Construct a mock HTTP request.
     req = func.HttpRequest(
         method='GET',
+        headers=CLIENT_HEADERS,
         body=None,
         url='/api/permit')
 
@@ -24,7 +28,7 @@ def test_permit_get_function():
     # Check the output.
     assert resp_json['status'] == 'success'
 
-def test_permit_get_function_request_error():
+def test_permit_get_function_request_error(mock_env_no_access_key):
     """ test_permit_get_function error """
 
     with patch('permit_get.func_json_response') as mock:
