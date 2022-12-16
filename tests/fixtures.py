297c0c94fe21e4e5c1dfdb2cf03dd3059ff60fad
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-12-16 15:16:28 -0800

    adding test fixtures

---
+++ tests/fixtures.py
@@ -0,0 +1,16 @@
+""" Test fixtures """
+import pytest
+
+CLIENT_HEADERS = {
+    "ACCESS_KEY": "1234567"
+}
+
+@pytest.fixture
+def mock_env_access_key(monkeypatch):
+    """ mock environment access key """
+    monkeypatch.setenv("ACCESS_KEY", CLIENT_HEADERS["ACCESS_KEY"])
+
+@pytest.fixture
+def mock_env_no_access_key(monkeypatch):
+    """ mock environment with no access key """
+    monkeypatch.delenv("ACCESS_KEY", raising=False)
