9b349bfbc85d9b4b99201ff88469e6c6117f2caf
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-12-21 14:27:17 -0800

    tests for status/pts

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,6 +9,11 @@ CLIENT_HEADERS = {
 def mock_env_access_key(monkeypatch):
     """ mock environment access key """
     monkeypatch.setenv("ACCESS_KEY", CLIENT_HEADERS["ACCESS_KEY"])
+    monkeypatch.setenv("PTS_USERNAME", "test")
+    monkeypatch.setenv("PTS_PASSWORD", "test")
+    monkeypatch.setenv("PTS_CONNECTSTRING", "127.0.0.1:1111/DB")
+    monkeypatch.setenv("ORACLE_LIB_DIR", "./lib/instantclient_19_8")
+    monkeypatch.setenv("LD_LIBRARY_PATH", "")
 
 @pytest.fixture
 def mock_env_no_access_key(monkeypatch):
