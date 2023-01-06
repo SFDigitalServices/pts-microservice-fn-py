763dffffdc508dcd470867ab31274bcd8adff3af
hshaosf <11635576+hshaosf@users.noreply.github.com>
2023-01-05 16:05:24 -0800

    permit_get init

--- permit_get/__init__.py
+++ permit_get/__init__.py
@@ -5,23 +5,58 @@ import logging
 import traceback
 import requests
 import jsend
+import oracledb
 
 import azure.functions as func
 from requests.models import Response
 from shared_code.common import func_json_response, validate_access, get_http_response_by_status
+from shared_code.oracle import get_oracle_connection
 
-#pylint: disable=unused-argument
+#pylint: disable=unused-argument, too-many-locals
 def main(req: func.HttpRequest) -> func.HttpResponse:
     """ main function for permit_get """
     logging.info('Permit GET processed a request.')
 
     try:
-        validate_access(req)
-        response = get_http_response_by_status(200)
+        response = Response()
+        json_root = "message"
+        out = None
         headers = {
             "Access-Control-Allow-Origin": "*"
         }
-        return func_json_response(response, headers, "message")
+
+        if req.params.get('avs_address_id'):
+            response.status_code = 200
+            sp_param = [
+                "P_AVS_ADDRESS_ID"
+            ]
+
+            connection = get_oracle_connection()
+            with connection.cursor() as cursor:
+                data_json =  {}
+                data_json["P_AVS_ADDRESS_ID"] = req.params.get('avs_address_id')
+
+                params = []
+                for field in sp_param:
+                    value = data_json[field] if field in data_json else ''
+                    params.append(value)
+
+                values = cursor.callfunc("pts.sp_rpt_get_filed_permits", oracledb.CURSOR, params)
+
+                for value in values:
+                    print(''.join(value)) # note: Do something with values
+
+                if values:
+                    json_root = "out"
+                    out = {"out": str(values)}
+
+            print(out)
+        else:
+            response.status_code = 200
+
+            out = "200 OK GET"
+
+        return func_json_response(response, headers, json_root, out)
 
     #pylint: disable=broad-except
     except Exception as err:
