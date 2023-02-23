6897d4fb16376a0b1e9880b983c2c700d9515bcd
josh-chou <626661+josh-chou@users.noreply.github.com>
2023-02-23 13:32:58 -0800

    raise exception when stored proc returns error (#9)
    * raise exception when stored proc returns error
    
    * change status to error
    
    * less confusing test
    
    * delete file

---
+++ tests/mocks/permit_post_stored_proc_response.py
@@ -0,0 +1,6 @@
+""" response from stored proc """
+error_response = {
+    "P_STATUS": "ERROR",
+    "P_MSG": "| Address not found",
+    "P_APP_NUM": ""
+}
--- tests/test_permit_post.py
+++ tests/test_permit_post.py
@@ -5,6 +5,7 @@ from unittest.mock import patch, Mock
 import azure.functions as func
 from tests.fixtures import mock_env_access_key, CLIENT_HEADERS, mock_cursor
 from permit_post import main
+import tests.mocks.permit_post_stored_proc_response as sp_mock
 
 @patch("permit_post.get_oracle_connection")
 def test_permit_post_function(mock_get_oracle_connection, mock_env_access_key):
@@ -40,6 +41,35 @@ def test_permit_post_function(mock_get_oracle_connection, mock_env_access_key):
     # Check the output.
     assert resp_json['status'] == 'success'
 
+@patch("permit_post.get_oracle_connection")
+def test_permit_post_function_address_not_found(mock_get_oracle_connection, mock_env_access_key):
+    """ test_permit_post_function when stored proc returns address not found error """
+
+    # mock body
+    with open('tests/mocks/permit_post_request.json', mode="rb") as file_handle:
+        mock_body = file_handle.read()
+
+    mock_side_effect = map(mock_cursor, sp_mock.error_response.values())
+    mock_get_oracle_connection.return_value\
+        .cursor.return_value.__enter__.return_value.var.side_effect\
+                = Mock(side_effect=mock_side_effect)
+
+    # Construct a mock HTTP request.
+    req = func.HttpRequest(
+        method='POST',
+        headers=CLIENT_HEADERS,
+        body=mock_body,
+        url='/api/permit')
+
+    # Call the function.
+    resp = main(req)
+    # print response body
+    print(resp.get_body())
+    # loads response body as json
+    resp_json = json.loads(resp.get_body())
+
+    # Check the output.
+    assert resp_json['status'] == 'error'
 
 def test_permit_post_function_other(mock_env_access_key):
     """ test_permit_post_function_other """
